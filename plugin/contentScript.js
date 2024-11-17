a9397d17a8b7f671e53a90b009009a35dbaa7b3e
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2024-11-17 11:42:31 +0100

    Added base URL fetching to the contentScript

--- plugin/contentScript.js
+++ plugin/contentScript.js
@@ -1,12 +1,21 @@
 /*
 contentScript.js 
 ----------------
-Extracts page text content and prints it in the console (for now)
+Extracts page text content and base URL and prints it in the console (for now)
 */
-console.log("Content script is running");
 
-// Directly get the text content of the entire page
+
+// FOR DEBUGGING PURPOSES, WILL BE REMOVED LATER
+console.log("Facture Extension: Content script is running");
+
+// GETS THE ENTIRE TEXT CONTENT FROM THE PAGE
 const pageText = document.body.innerText || document.body.textContent;
 
-// Log it in the console
-console.log("Page text content:", pageText);
+// LOGS TEXT CONTENT INTO THE CONSOLE
+console.log("Facture Extension: Page text content:", pageText);
+
+// GETS BASE URL
+const baseUrl = window.location.origin + '/';
+
+// LOGS BASE URL INTO THE CONSOLE
+console.log("Facture Extension: Page base URL: ", baseUrl);
